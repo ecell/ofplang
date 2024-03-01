507fa3a1728b22cd8fa08bc61d3c343f9998f4d6
kaizu <-C>
2024-03-01 18:35:12 +0900

    Add validate

--- sample.py
+++ sample.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from protocol import Protocol
+from validate import check_protocol
 
 import sys
 
 
 protocol = Protocol("./sample.yaml")
 print(list(protocol.connections()))
-# protocol.save(sys.stdout)
+protocol.save(sys.stdout)
+
+check_protocol(protocol)
---
+++ validate.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+import itertools
+
+from protocol import Protocol
+
+
+def check_protocol(protocol: Protocol) -> None:
+    check_unique_id(protocol)
+    check_connection_port_exists(protocol)
+
+def check_unique_id(protocol: Protocol) -> None:
+    is_valid = True
+    id_list = []
+    for entity in itertools.chain(protocol.input(), protocol.output()):
+        if entity.id not in id_list:
+            id_list.append(entity.id)
+        else:
+            logger.error(f"Id [{entity.id}] already exists.")
+            is_valid = False
+    
+    id_list = ["input", "output"]
+    for entity in protocol.operations():
+        if entity.id not in id_list:
+            id_list.append(entity.id)
+        else:
+            logger.error(f"Id [{entity.id}] already exists.")
+            is_valid = False
+
+    assert is_valid, "Id must be unique."
+
+def check_connection_port_exists(protocol: Protocol) -> None:
+    id_list = ["input", "output"]
+    for entity in protocol.operations():
+        if entity.id not in id_list:
+            id_list.append(entity.id)
+
+    is_valid = True
+    for connection in protocol.connections():
+        if connection.input.operation_id not in id_list:
+            logger.error(f"Id [{connection.input.operation_id}] does not exist.")
+            is_valid = False
+        if connection.output.operation_id not in id_list:
+            logger.error(f"Id [{connection.output.operation_id}] does not exist.")
+            is_valid = False
+
+    assert is_valid, "Invalid connection."
