9f514430d095e8272a817a4ae5591f85abefce3f
kaizu <-C>
2024-12-16 12:30:13 +0900

    #6 Dump outputs as json

--- src/ofplang/scripts/cli.py
+++ src/ofplang/scripts/cli.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from logging import getLogger
+import sys
 import pathlib
+import numpy
 import yaml
 import click
+import json
 
 from ofplang.prelude import Runner
 from ofplang.executors import Simulator
 
 logger = getLogger(__name__)
+
+def ndarray_representer(dumper: yaml.Dumper, array: numpy.ndarray) -> yaml.Node:
+    return dumper.represent_list(array.tolist())
+
+yaml.add_representer(numpy.ndarray, ndarray_representer)
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return super().default(obj)
 
 
 @click.group()
@@ -19,23 +33,30 @@ def cli() -> None:
 @click.argument('protocol')
 @click.option("--definitions", "-d", help="The definition YAML file", default=None)
 @click.option("--cli-input-yaml", default=None)
-def run(protocol: str, definitions: str | None, cli_input_yaml: str | None) -> None:
-    click.echo(f"Run protocol [{protocol}] with definitions [{definitions}]")
+@click.option("--format", default="json")
+def run(protocol: str, definitions: str | None, cli_input_yaml: str | None, format: str) -> None:
+    logger.debug(f"Run protocol [{protocol}] with definitions [{definitions}]")
 
     if definitions is None:
         definitions = "./definitions.yaml"
     runner = Runner(protocol, definitions, executor=Simulator())
     
-    click.echo(f"Parse inputs [{cli_input_yaml}]")
+    logger.debug(f"Parse inputs [{cli_input_yaml}]")
     if cli_input_yaml is not None:
         with pathlib.Path(cli_input_yaml).open() as f:
             inputs = yaml.load(f, Loader=yaml.Loader)
     else:
         inputs = {}
 
-    click.echo(f"Input value: {inputs}")    
+    logger.debug(f"Input value: {inputs}")
     outputs = runner.run(inputs).output
-    click.echo(f"Output value: {outputs}")
+    logger.debug(f"Output value: {outputs}")
+
+    if format.lower() == "yaml":
+        yaml.dump(outputs, sys.stdout, indent=2)
+    else:
+        # assert format.lower() == "json", format
+        json.dump(outputs, sys.stdout, cls=NumpyEncoder, indent=2)
 
 def main() -> None:
     cli()
