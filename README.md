b1e8ad511b8b611c96c7f0683c9aa73b63b19b8b
kaizu <-C>
2024-12-16 11:59:10 +0900

    #6 Enable to run

--- README.md
+++ README.md
@@ -14,4 +14,5 @@ $ poetry run ruff check ofplang
 $ poetry run mypy ofplang
 $ cd samples
 $ poetry run python sample1.py
+$ poetry run ofplang run protocol1.yaml --definitions definitions.yaml --cli-input-yaml inputs1.yaml
 ```
--- src/ofplang/scripts/cli.py
+++ src/ofplang/scripts/cli.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from logging import getLogger
-
+import pathlib
+from typing import IO
+import yaml
 import click
+
+from ofplang.prelude import *
+from ofplang.executors import Simulator
 
 logger = getLogger(__name__)
 
 
 @click.group()
-def cli():
+def cli() -> None:
     pass
 
-@cli.command()
-def run():
-    pass
+@cli.command(help="Run a protocol")
+@click.argument('protocol')
+@click.option("--definitions", "-d", help="The definition YAML file", default=None)
+@click.option("--cli-input-yaml", default=None)
+def run(protocol: str, definitions: str | None, cli_input_yaml: str | None) -> None:
+    click.echo(f"Run protocol [{protocol}] with definitions [{definitions}]")
 
-def main():
+    if definitions is None:
+        definitions = "./definitions.yaml"
+    runner = Runner(protocol, definitions, executor=Simulator())
+    
+    click.echo(f"Parse inputs [{cli_input_yaml}]")
+    if cli_input_yaml is not None:
+        with pathlib.Path(cli_input_yaml).open() as f:
+            inputs = yaml.load(f, Loader=yaml.Loader)
+    else:
+        inputs = {}
+
+    click.echo(f"Input value: {inputs}")    
+    outputs = runner.run(inputs).output
+    click.echo(f"Output value: {outputs}")
+
+def main() -> None:
     cli()
 
 
